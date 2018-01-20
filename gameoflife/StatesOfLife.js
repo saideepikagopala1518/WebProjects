a2c16bcce6dfbac583300c28f1615aa2f9865d78
saideepikagopala1518 <sgopala1@student.gsu.edu>
2018-01-19 20:58:46 -0500

    Add files via upload

---
+++ gameoflife/StatesOfLife.js
@@ -0,0 +1,9 @@
+/**
+@author: Sai Deepika Gopala
+ * "Enum" to make understanding the rules of the game easier.
+ * @type {{DEAD: number, ALIVE: number, RESURRECTED: number, LIVES_ON: number}}
+ */
+var StatesOfLife = {
+    DEAD: 0,
+    ALIVE: 1
+};
